b8ff411b81bcc658f9678aec921f6fb6ff10783b
shubhO4 <155084951+shubhO4@users.noreply.github.com>
2024-01-25 21:20:03 +0530

    added loginpage

---
+++ src/login page/login.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./style.css";
+
+export const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <img className="image" alt="Image" src="https://c.animaapp.com/O6jmtek0/img/image-4.png" />
+          <div className="group">
+            <div className="div">
+              <div className="group-2">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">Login</div>
+                    </div>
+                    <div className="text-wrapper-2">Register</div>
+                  </div>
+                </div>
+                <div className="welcome-to-pecunia">Welcome to&nbsp;&nbsp;Pecunia</div>
+              </div>
+              <div className="div">
+                <div className="overlap-2">
+                  <div className="group-3">
+                    <img
+                      className="rectangle"
+                      alt="Rectangle"
+                      src="https://c.animaapp.com/O6jmtek0/img/rectangle-80.svg"
+                    />
+                    <div className="user-name"> User name</div>
+                    <div className="text-wrapper-3">Enter your User name</div>
+                  </div>
+                  <div className="group-4">
+                    <div className="overlap-3">
+                      <div className="group-5">
+                        <div className="overlap-group-2">
+                          <div className="text-wrapper-4">Enter your Password</div>
+                        </div>
+                        <div className="text-wrapper-5">Password</div>
+                        <div className="text-wrapper-6">Enter your Username</div>
+                      </div>
+                      <img
+                        className="invisible"
+                        alt="Invisible"
+                        src="https://c.animaapp.com/O6jmtek0/img/invisible-1.svg"
+                      />
+                    </div>
+                  </div>
+                  <p className="lorem-ipsum-is">
+                    {" "}
+                    Lorem Ipsum is simply dummy text of the printing and&nbsp;&nbsp;
+                    <br /> typesetting industry.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="group-6">
+              <div className="rectangle-2" />
+              <div className="text-wrapper-7">Rememebr me</div>
+              <div className="text-wrapper-8">Forgot Password ?</div>
+            </div>
+            <div className="group-7">
+              <div className="overlap-4">
+                <div className="text-wrapper-9">Login</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};

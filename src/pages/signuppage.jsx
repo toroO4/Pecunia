48c61789fc4b0cc4f62335afd6cfe8119cba67f1
Toro Tisekar <adyantisekar04@gmail.com>
2024-01-27 19:04:07 +0530

    added signup details

--- src/pages/signuppage.jsx
+++ src/pages/signuppage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-
 export const SignupPage = () => {
   return (
     <div className="signup-page">
       <div className="div">
         <div className="text-wrapper">Create Account</div>
-        <img className="la-facebook" alt="La facebook" src="la-facebook.svg" />
-        <img className="icomoon-free-google" alt="Icomoon free google" src="icomoon-free-google-plus3.svg" />
-        <img className="entypo-social" alt="Entypo social" src="entypo-social-linkedin-with-circle.svg" />
+        <img className="facebook-logo" alt="La facebook" src="./assets/images/facebook.svg" />
+        <img className="google-logo" alt="Icomoon free google" src="./assets/images/google-plus.svg" />
+        <img className="linkedin-logo" alt="Entypo social" src="./assets/images/linkedIn.svg" />
         <p className="p">or use your email account</p>
         <div className="overlap">
           <div className="text-wrapper-2">Enter your name</div>
@@ -24,18 +23,18 @@ export const SignupPage = () => {
         </div>
         <div className="text-wrapper-4">Forgot your password?</div>
         <div className="overlap-2">
-          <img className="carbon-logo-slack" alt="Carbon logo slack" src="carbon-logo-slack.svg" />
+          <img className="slack-logo" alt="LOGO" src=""/>
           <div className="overlap-3">
             <div className="overlap-group-wrapper">
               <div className="overlap-group-2">
                 <div className="text-wrapper-5">Sign In</div>
               </div>
             </div>
+            <div className="text-wrapper-6">Already have an account?</div>
           </div>
+          <div className="text-wrapper-7">Start New Journey!</div>
         </div>
-        <div className="text-wrapper-6">Already have an account?</div>
       </div>
-      <div className="text-wrapper-7">Start New Journey!</div>
     </div>
   );
 };

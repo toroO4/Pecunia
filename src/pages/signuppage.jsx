3aa3d2ec816b9d178820e32210ff0d0d340d5320
shubhO4 <155084951+shubhO4@users.noreply.github.com>
2024-01-27 16:00:55 +0530

    add signuppage

--- src/pages/signuppage.jsx
+++ src/pages/signuppage.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
+import "./style.css";
 
-export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
-
-    return (
-        <div className="auth-form-container">
-            <h2>Register</h2>
-        <form className="register-form" onSubmit={handleSubmit}>
-            <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
-        </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+export const SignupPage = () => {
+  return (
+    <div className="signup-page">
+      <div className="div">
+        <div className="text-wrapper">Create Account</div>
+        <img className="la-facebook" alt="La facebook" src="la-facebook.svg" />
+        <img className="icomoon-free-google" alt="Icomoon free google" src="icomoon-free-google-plus3.svg" />
+        <img className="entypo-social" alt="Entypo social" src="entypo-social-linkedin-with-circle.svg" />
+        <p className="p">or use your email account</p>
+        <div className="overlap">
+          <div className="text-wrapper-2">Enter your name</div>
+        </div>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">Enter your name</div>
+        </div>
+        <div className="div-wrapper">
+          <div className="text-wrapper-2">Enter your name</div>
+        </div>
+        <div className="frame">
+          <div className="text-wrapper-3">Sign Up</div>
+        </div>
+        <div className="text-wrapper-4">Forgot your password?</div>
+        <div className="overlap-2">
+          <img className="carbon-logo-slack" alt="Carbon logo slack" src="carbon-logo-slack.svg" />
+          <div className="overlap-3">
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group-2">
+                <div className="text-wrapper-5">Sign In</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-wrapper-6">Already have an account?</div>
+      </div>
+      <div className="text-wrapper-7">Start New Journey!</div>
     </div>
-    )
-}
+  );
+};

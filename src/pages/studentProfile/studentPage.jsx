ad846584195914e0af3356badef8163f976a8c09
Toro Tisekar <adyantisekar04@gmail.com>
2024-02-10 16:01:42 +0530

    add

--- src/pages/studentProfile/studentPage.jsx
+++ src/pages/studentProfile/studentPage.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import "./Home.css";
-
+import "./Home.css"; // Import the CSS file for styling
 import Leftside from "./Leftside";
 import Main from "./Main";
 import Rightside from "./Rightside";
 
 const Home = (props) => {
   return (
-    <div className="home-container">
-      <section className="home-section">
+    <div className="container">
+      <section className="section">
         <h5>
           <a>Hiring in a hurry? - </a>
         </h5>
@@ -18,7 +17,7 @@ const Home = (props) => {
           moving.
         </p>
       </section>
-      <div className="home-layout">
+      <div className="layout">
         <Leftside />
         <Main />
         <Rightside />

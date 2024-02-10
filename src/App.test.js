e15883494280c21b3ff64e5008c48dd9aec3fc87
Toro Tisekar <adyantisekar04@gmail.com>
2024-02-10 16:28:48 +0530

    add

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import '../backend/server';
+// import '../backend/server';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../backend/server'
+// import '../../backend/server'
 
 
 export const Login = () => {
